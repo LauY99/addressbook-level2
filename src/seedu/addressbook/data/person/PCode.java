ee99b0cc8d69bdc8f6f85222c61ba552542ffcda
Lau Yung <lauy1997@gmail.com>
2017-09-07 10:19:12 +0800

    Add classes Block, Street, Unit, PCode

---
+++ src/seedu/addressbook/data/person/PCode.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+public class PCode {
+
+    public final String value;
+
+    public PCode(String address) {
+        this.value = address;
+    }
+}
---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+
+    public final String value;
+
+    public Street(String address) {
+        this.value = address;
+    }
+}
